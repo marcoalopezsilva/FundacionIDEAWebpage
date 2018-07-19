15c90d021cf616187b77898a3f4b976f40b45827
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-07-18 20:53:23 -0500

    Trying to roll back to simple seed, will manually enter publications

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,28 +16,6 @@ editor_user.skip_confirmation!
 admin_user.save
 editor_user.save
 
-doc1 = Publication.new(
-    title: "México Compacto",
-    description: "Esta publicación revisa los conceptos internacionales de ciudad compacta y densificación urbana a la vez que establece siete condiciones para que una estrategia de este tipo se ponga en práctica de manera positiva en México y, particularmente, en la Ciudad de México. Una de estas condiciones es responder a la informalidad y los asentamientos irregulares. Otra condición es promover la participación pública en el desarrollo urbano.",
-    tag: "Desarrollo Urbano",
-    pub_date: "2016-06-14",
-    user: User.order("RANDOM()").first
-)
-=begin
-doc1.image.attach(
-   io: File.open('app/assets/images/MexicoCompactoPortada.jpg'),
-   filename: 'MexicoCompactoPortada.jpg',
-   content_type: 'image/jpeg'
-)
-doc1.document.attach(
-   io: File.open('app/assets/images/MexicoCompactoDocumento.pdf'),
-   filename: 'MexicoCompactoDocumento.pdf',
-   content_type: 'application/pdf'
-)
-=end
-doc1.save
-
 
 puts "Seed finished"
 puts "#{User.count} users created"
-puts "#{Publication.count} publications created"
